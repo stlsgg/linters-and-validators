62fb94df63c8561356c3db97e4f305b588ae56c9
stlsgg <gregoryneguzyak.gaming@gmail.com>
2025-10-05 11:21:47 +0300

    feat: animation for sidebar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,13 +1,32 @@
+import { animate, linear } from "animejs";
+import { spring } from "animejs";
+
 // Sidebar logic, functions, etc.
 function initSidebar() {
 	const sidebar = document.getElementById("sidebar");
 	const sbBtn = document.getElementById("sidebar-button");
+	const sbBtnSvg = sbBtn.querySelector("svg");
 
 	if (!sidebar || !sbBtn) return;
 
+	let isActive = false;
+	const hiddenRight = "-240px";
+	const shownRight = "0px";
+
 	sbBtn.addEventListener("click", () => {
-		sidebar.classList.toggle("sidebar");
-		sidebar.classList.toggle("sidebar--active");
+		isActive = !isActive;
+
+		animate(sidebar, {
+			right: isActive ? shownRight : hiddenRight,
+			duration: 100,
+			ease: spring({ bounce: 0.2 }),
+		});
+
+		animate(sbBtnSvg, {
+			rotate: isActive ? 180 : 0,
+			duration: 500,
+			ease: spring({ bounce: 0.6 }),
+		});
 	});
 }
 
