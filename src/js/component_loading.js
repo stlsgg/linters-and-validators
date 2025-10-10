3fd173c5e22b9fde537d20fd6d53a9a7e974ec35
stlsgg <gregoryneguzyak.gaming@gmail.com>
2025-10-10 20:23:34 +0300

    changes

--- src/js/component_loading.js
+++ src/js/component_loading.js
@@ -2,18 +2,20 @@ import * as mainUtils from "./funcs.js";
 import * as headerUtils from "./header.js";
 import { initSidebar } from "./sidebar.js";
 
+const baseUrl = import.meta.env.BASE_URL;
+
 // Header loading
 const header = document.querySelector("header");
 mainUtils
-	.loadComponent("./layout/header.html", header)
+	.loadComponent(`${baseUrl}layout/header.html`, header)
 	.then(() => headerUtils.initBurgerMenu());
 
 // Footer loading
 const footer = document.querySelector("footer");
-mainUtils.loadComponent("./layout/footer.html", footer);
+mainUtils.loadComponent(`${baseUrl}layout/footer.html`, footer);
 
 // Sidebar loading
 const sidebar = document.getElementById("sidebar");
 mainUtils
-	.loadComponent("./layout/sidebar.html", sidebar)
+	.loadComponent(`${baseUrl}layout/sidebar.html`, sidebar)
 	.then(() => initSidebar());
